e8c18a5568c5ceb114e5e770af24ce7ea8e19389
Marcin Dołżański <marcin.dolzanski@wp.pl>
2020-11-28 22:14:37 +0100

    Initial Commit

---
+++ bot/bot.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import random
+teksty = [" to śmieć"," jest jebaną pałą"," chuj ci na starego"]
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content.find("rudy.losuj") != -1:
+        players = message.content
+        players = str(players)[11:].split(' ')
+        if(len(players)==10):
+            random.shuffle(players)
+            response = "**Team1**\n"+players[0]+"\n"+players[1]+"\n"+players[2]+"\n"+players[3]+"\n"+players[4]+"\n"+"\n"+"**Team2**\n"+players[5]+"\n"+players[6]+"\n"+players[7]+"\n"+players[8]+"\n"+players[9]+""
+            await message.channel.send(response)
+        else:
+            await message.channel.send("Ma być 10 zawodników pajacu")
+
+    elif message.content.find("rudy to pizda") != -1:
+        author = (str(message.author))[:-5]
+        random.shuffle(teksty)
+        await message.channel.send(author+teksty[0])
+
+    elif message.content.find("bajo jajo") != -1:
+        await message.channel.send("Ty pojebie kurwa się zamknij tam kurwa a nie bajoo jajo ty pierdolony chamie")
+        
+client.run("NzgyMjA3NDE0MzY2ODk2MTI4.X8I1bA.v5vO8zqTSznlnTWOiZTwukQv8Ek")
+
+
